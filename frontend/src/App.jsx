e7c182b784e4456e3beb4941a29b49ff626910f9
aryan-ya <ay3978943@gmail.com>
2024-08-10 13:02:35 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import {Toaster} from "react-hot-toast";
+import Home from "./Pages/Home";
+import Success from "./Pages/Success";
+import Notfound from "./Pages/Notfound";
 const App = () => {
   return  <Router>
     <Routes>
-    <Route path="/" element= {}/>
-    <Route path="/" element={}/>
-    <Route path="/" element={}/>
+    <Route path="/" element= {<Home />}/>
+    <Route path="/" element={<Success />}/>
+    <Route path="/" element={<Notfound />}/>
 
     </Routes>
     <Toaster/>
