de136c08950f85ccd1c7d8708fa61b88360fc0cb
aryan-ya <ay3978943@gmail.com>
2024-08-24 12:21:18 +0530

    frontend

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 
+import {Link} from "react-router-dom";
+import {HiOutlineArrowNarrowRight} from "react-icons/hi"
 //about
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='about' id='about'>
+      <div className="container">
+        <div className="banner">
+          <div className="top">
+            <h1 className="heading">ABOUT US</h1>
+            <p>The only thing we are serious about is food.</p>
+          </div>
+          <p className="mid">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, consectetur veritatis ad natus hic nesciunt tempora ullam repudiandae neque quia, repellat nihil? Cum tempora velit quas cupiditate ea dolorem consectetur libero enim saepe, sapiente, aliquid placeat eligendi in! Illo laborum aut quidem repellat dolor velit ratione eius consectetur possimus aliquid?
+          </p>
+         <Link to={"/"}>Explore Menu
+         <span></span>
+         </Link>
+        </div>
+      </div>
+    </section>
   )
 }
 
